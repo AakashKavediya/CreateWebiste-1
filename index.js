e2f855893c85aea031d60f180c3dd9b0204624eb
Aakash Kavediya <kavediyaaakash@gmail.com>
2022-06-10 15:42:40 +0530

    Push

---
+++ index.js
@@ -0,0 +1,48 @@
+var header = 0;
+
+function heade() {
+    header = String(document.getElementById("n1").value);
+    document.getElementById("header").innerHTML = header;
+}
+
+var para1 = 0;
+
+function paraone() {
+    para1 = String(document.getElementById("n2").value);
+    document.getElementById("para1").innerHTML = para1;
+}
+
+var para2 = 0;
+
+function paratwo() {
+    para2 = String(document.getElementById("n3").value);
+    document.getElementById("para2").innerHTML = para2;
+}
+
+var para3 = 0;
+
+function parathree() {
+    para3 = String(document.getElementById("n4").value);
+    document.getElementById("para3").innerHTML = para3;
+}
+
+var para4 = 0;
+
+function parafour() {
+    para4 = String(document.getElementById("n5").value);
+    document.getElementById("para4").innerHTML = para4;
+}
+
+var para5 = 0;
+
+function parafive() {
+    para5 = String(document.getElementById("n6").value);
+    document.getElementById("para5").innerHTML = para5;
+}
+
+var para6 = 0;
+
+function parasix() {
+    para6 = String(document.getElementById("n7").value);
+    document.getElementById("para6").innerHTML = para6;
+}
